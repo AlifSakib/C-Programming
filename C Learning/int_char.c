66cc818b03bac71fd6ecf3d812e2f89c446003b7
AlifSakib <alifsakib@gmail.com>
2023-01-27 18:03:50 +0600

    while loop

--- C Learning/int_char.c
+++ C Learning/int_char.c
@@ -61,17 +61,40 @@ int main()
 
     // return 0;
 
+    // int n;
+
+    // scanf("%d", &n);
+    // if (n > 10)
+    // {
+    //     printf("%d is greater theb 10\n", n);
+    // }
+    // if (n > 5)
+    // {
+    //     printf("%d is greater then 5\n", n);
+    // }
+
+    // return 0;
+
+    // int marks;
+    // char grade;
+
+    // scanf("%d", &marks);
+
+    // if (marks >= 80)
+    // {
+    //     grade = 'A';
+    //     printf("Your grade in %c\n", grade);
+    // }
+
+    // int n;
+
+    // n = 1;
+    // while (n <= 10)
+    // {
+    //     printf("%d\n", n);
+    //     n = n + 1;
+    // }
+
     int n;
-
-    scanf("%d", &n);
-    if (n > 10)
-    {
-        printf("%d is greater theb 10\n", n);
-    }
-    if (n > 5)
-    {
-        printf("%d is greater then 5\n", n);
-    }
-
-    return 0;
+    n = 1;
 }
